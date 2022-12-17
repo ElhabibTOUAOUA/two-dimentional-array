6783d9d26e6569ea48844cd9d24a8f25cb850f34
Elhabib TOUAOUA <bibahhabib8@gmail.com>
2022-12-17 15:45:56 +0100

    adding user matrix input

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
 import numpy as np
 import hashlib
 
-a = [    
-      [1, 0, 1, 1],  
-      [1, 1, 1, 0],  
-      [0, 0, 1, 0],  
-      [1, 1, 0, 0],  
-    ]  
+# a = [    
+#       [1, 0, 1, 1],  
+#       [1, 1, 1, 0],  
+#       [0, 0, 1, 0],  
+#       [1, 1, 0, 0],  
+#     ]  
 
-b = [    
-      [1, 0, 1, 1],  
-      [1, 1, 1, 0],  
-      [0, 0, 1, 0],  
-      [1, 1, 0, 1],  
-    ]  
+def inputMatrixFromUser(rows, cols):
+  # get how many rows and columns from user
+  matrix = []
+  for i in range(rows):
+    matrix.append([])
+    for j in range(cols):
+      matrix[i].append(int(input(f"Enter the value of row {i+1} and column {j+1}: ")))
+  return matrix
 
-def list_sum(list):
+
+def arraySum(list):
   sum = 0
   for i in range(len(list)):
     sum += list[i]
   return sum
 
 
-def hash_arr(two_d_arr):
+def matrixHash(two_d_arr):
   two_d_arr = np.matrix(two_d_arr)
   return hashlib.sha1(two_d_arr).hexdigest()
 
 
-def arr_gen():
-  row, col = 4, 4
-  matrix = np.random.randint(2, size=(row,col))
+def matrixGenerator(rows, cols):
+  matrix = np.random.randint(2, size=(rows,cols))
   return matrix
 
 
-def verctors(two_d_arr):
+def verctorsExtraction(two_d_arr):
   v1, v2 = [], []
   for i in range(len(two_d_arr)):
-    v1.append(list_sum(two_d_arr[i]))
+    v1.append(arraySum(two_d_arr[i]))
     sum_col = 0
     for j in range(len(two_d_arr[i])):
       sum_col += two_d_arr[j][i]
@@ -46,20 +48,32 @@ def verctors(two_d_arr):
 
 
 if __name__ == "__main__":
-
-  a_v1, a_v2 = verctors(a)
-  a_hash = hash_arr(a)
+  rows, cols = int(input("Enter the number of rows: ")), int(input("Enter the number of columns: "))
+  inputMatrix = inputMatrixFromUser(rows, cols)
+  a_v1, a_v2 = verctorsExtraction(inputMatrix)
+  inputMatrixHash = matrixHash(inputMatrix)
+  numOfGeneratedMatrix = 0
+  numOfGeneratedMatrixWithSameVectors = 0
   x = True
   while x:
-    b = arr_gen()
-    # print(np.matrix(b))
-    b_v1, b_v2 = verctors(b)
+    generatedMatrix = matrixGenerator(rows, cols)
+    # print(np.matrix(generatedMatrix))
+    numOfGeneratedMatrix += 1
+    b_v1, b_v2 = verctorsExtraction(generatedMatrix)
     if b_v1 == a_v1 and b_v2 == a_v2:
-      print("***** Same Vectors *****")
-      print(np.matrix(b))
-      if a_hash == hash_arr(b):
+      numOfGeneratedMatrixWithSameVectors += 1
+      # print("***** Same Vectors *****")
+      # print(np.matrix(generatedMatrix))
+      if inputMatrixHash == matrixHash(generatedMatrix):
         print("***** Same Vectors and Hash *****")
-        print(np.matrix(b))
+        print(np.matrix(generatedMatrix))
         print(f"v1: {b_v1} \nv2: {b_v2}")
+        print(f"Input Matrix hash: {inputMatrixHash} \nGenerated Matrix hash: {matrixHash(generatedMatrix)}")
         x = False
-
+    #   else:
+    #     # print("***** Same Vectors but Different Hash *****")
+    # else:
+    #   # print("***** Different Vectors *****")
+    
+  print(f"Number of Generated Matrix: {numOfGeneratedMatrix}")
+  print(f"Number of Generated Matrix with Same Vectors: {numOfGeneratedMatrixWithSameVectors}")
